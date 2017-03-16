e58e2a9c143fae914d779ced03e81e4a95ed8def
Sam Ye <u5824077@anu.edu.au>
2017-03-17 02:54:56 +1100

    timetable update

--- js/timetable.js
+++ js/timetable.js
@@ -2,8 +2,8 @@ var rawLessons      = [];
 var timetableData   = {};
 var hasLocalStorage = typeof(Storage) !== 'undefined';
 var recover         = false;
-var jsonUpdatedTime = '1st of March, 2017';
-var revisionNum     = 18;
+var jsonUpdatedTime = '17th of March, 2017';
+var revisionNum     = 19;
 
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (value) {
