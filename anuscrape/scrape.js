207310603f4a2606f1ed82e02a02aa603ab48f46
Emmanuel <xtrememanos@windowslive.com>
2017-07-21 15:11:59 +1000

    Update scrape.js

--- anuscrape/scrape.js
+++ anuscrape/scrape.js
@@ -42,7 +42,7 @@ if (jQuery('#tFilterTitle').html().indexOf('Course') !== -1) {
                 'dlObject'            : courseElement.value,
 				// Note this should be changed depending on the semester:
 				// 29-46 for semester 2
-                'lbWeeks'             : '1-52',
+                'lbWeeks'             : '29-46',
                 'lbDays'              : '1-7;1;2;3;4;5;6;7',
                 'dlPeriod'            : '1-32;1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;21;22;23;24;25;26;27;28;29;30;31;32;',
                 'RadioType'           : 'module_list;cyon_reports_list_url;dummy',
