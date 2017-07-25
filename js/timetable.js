ff50bb43aac0e95925c0a813efafa68ef1f20c8d
Emmanuel Morfiadakis <u5587023@anu.edu.au>
2017-07-25 13:28:03 +1000

    Update 25/7

--- js/timetable.js
+++ js/timetable.js
@@ -2,8 +2,8 @@ var rawLessons      = [];
 var timetableData   = {};
 var hasLocalStorage = typeof(Storage) !== 'undefined';
 var recover         = false;
-var jsonUpdatedTime = '23rd of July, 2017';
-var revisionNum     = 25;
+var jsonUpdatedTime = '25th of July, 2017';
+var revisionNum     = 26;
 
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (value) {
