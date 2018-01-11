2dd3376878b6131140d14587ef6d3a04ee812bb8
Emmanuel <xtrememanos@windowslive.com>
2018-01-12 00:37:46 +1100

    Hopefully this fixed the latest issue with courses not appearing correctly. Updated the scrapers, added a debugging python script, and updated the timetable to its latest version.

--- js/timetable.js
+++ js/timetable.js
@@ -2,8 +2,8 @@ var rawLessons      = [];
 var timetableData   = {};
 var hasLocalStorage = typeof(Storage) !== 'undefined';
 var recover         = false;
-var jsonUpdatedTime = '5th of January, 2018';
-var revisionNum     = 32;
+var jsonUpdatedTime = '12th of January, 2018';
+var revisionNum     = 33;
 
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (value) {
