f91ed4c922a066fa325bcaeaf5f0437469b656e7
Emmanuel <xtrememanos@windowslive.com>
2018-01-16 00:56:19 +1100

    Updating website with new timetable (#184)
    * Merging auto_update branch and keeping relevant files only
    * Changed readme file

--- js/timetable.js
+++ js/timetable.js
@@ -2,8 +2,8 @@ var rawLessons      = [];
 var timetableData   = {};
 var hasLocalStorage = typeof(Storage) !== 'undefined';
 var recover         = false;
-var jsonUpdatedTime = '12th of January, 2018';
-var revisionNum     = 33;
+var jsonUpdatedTime = '15th of January, 2018';
+var revisionNum     = 36;
 
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (value) {
