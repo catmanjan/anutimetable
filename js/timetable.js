1c411b7a043969c2b0719ef3e02cb11d1dda86b7
Emmanuel <xtrememanos@windowslive.com>
2018-01-24 23:10:23 +1100

    Updating Timetable (#187)
    * Fixed README and moved files to better locations
    
    * Fixed README and moved files to better locations again
    
    * Updated README.MD
    
    Added server details
    
    * Update README.md
    
    Fixed wrong dates on update log and added meaningful descriptions of files
    
    * Updated on Wed Jan 17 17:40:35 AEDT 2018
    
    * Updated on Fri Jan 19 17:40:33 AEDT 2018
    
    * Updated on Tue Jan 23 17:40:25 AEDT 2018

--- js/timetable.js
+++ js/timetable.js
@@ -2,8 +2,8 @@ var rawLessons      = [];
 var timetableData   = {};
 var hasLocalStorage = typeof(Storage) !== 'undefined';
 var recover         = false;
-var jsonUpdatedTime = '15th of January, 2018';
-var revisionNum     = 36;
+var jsonUpdatedTime = '23rd of January, 2018';
+var revisionNum     = 39;
 
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (value) {
