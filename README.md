b502b45e58bab8e8d2820b1257c588463d8df288
EC2 Default User <ec2-user@ip-172-31-2-93.ap-southeast-2.compute.internal>
2018-04-20 17:38:53 +1000

    Updated on Fri Apr 20 17:38:53 AEST 2018

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # anutimetable
 
-* Timetable database last update: 2018-04-18
+* Timetable database last update: 2018-04-20
 
 ## Updates
 
--- js/timetable.js
+++ js/timetable.js
@@ -2,8 +2,8 @@ var rawLessons      = [];
 var timetableData   = {};
 var hasLocalStorage = typeof(Storage) !== 'undefined';
 var recover         = false;
-var jsonUpdatedTime = '18th of April, 2018';
-var revisionNum     = 95;
+var jsonUpdatedTime = '20th of April, 2018';
+var revisionNum     = 96;
 
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (value) {
