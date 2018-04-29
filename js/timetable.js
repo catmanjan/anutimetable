31080d54a3a0e5ea88507bd75ba23f54b437b04e
EC2 Default User <ec2-user@ip-172-31-2-93.ap-southeast-2.compute.internal>
2018-04-29 17:38:23 +1000

    Updated on Sun Apr 29 17:38:23 AEST 2018

--- js/timetable.js
+++ js/timetable.js
@@ -2,8 +2,8 @@ var rawLessons      = [];
 var timetableData   = {};
 var hasLocalStorage = typeof(Storage) !== 'undefined';
 var recover         = false;
-var jsonUpdatedTime = '27th of April, 2018';
-var revisionNum     = 101;
+var jsonUpdatedTime = '29th of April, 2018';
+var revisionNum     = 102;
 
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (value) {
