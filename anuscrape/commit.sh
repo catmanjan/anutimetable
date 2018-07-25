d294ff8ff83a71180a2860623186d5fd0ce6e61f
mdchia <u5494149@anu.edu.au>
2018-07-25 19:26:35 +1000

    move to relative refs

--- anuscrape/commit.sh
+++ anuscrape/commit.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 # TODO add basic instructions so anyone can replicate the server
+echo Starting ...
 
 # Check if the newly scraped file does not exist. Print meaningful message and exit with failure
-if [ ! -e $HOME/timetable.json ]; then
-	echo Scraping failed at `date` >> $HOME/updatelog.txt
-	exit 1
+if [ ! -e ./timetable.json ]; then
+    echo Scraping failed at `date` >> ./updatelog.txt
+    exit 1
 fi 
 
 # Compare the newly downloaded timetable with the previous one
-diff $HOME/timetable.json $HOME/anutimetable/data/timetable.json
+diff ./timetable.json ../data/timetable.json
 
 # If new one is different or the old one does not exist, commit new one to GitHub
 if [ $? -ne 0 ]; then
     echo New update found at `date` >> $HOME/updatelog.txt
-    mv -f $HOME/timetable.json $HOME/anutimetable/data/
-    cd $HOME/anutimetable/
-    python3.6 $HOME/anutimetable/anuscrape/update_date.py
+    mv -f ./timetable.json ../data/
+    python3 ./update_date.py
     git add *
     git commit -m "Updated on `date`"
     git push
     exit 0
 else 
-	echo No changes on `date` >> $HOME/updatelog.txt
-	exit 1
+    echo No changes on `date` >> ./updatelog.txt
+    exit 1
 fi
