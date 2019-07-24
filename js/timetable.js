55991f68dbf9b106601de94e9194e5d4db329142
mdchia <u5494149@anu.edu.au>
2019-07-24 20:24:08 +1000

    rescrape

--- js/timetable.js
+++ js/timetable.js
@@ -2,8 +2,8 @@ var rawLessons      = [];
 var timetableData   = {};
 var hasLocalStorage = typeof(Storage) !== 'undefined';
 var recover         = false;
-var jsonUpdatedTime = '19th of July, 2019';
-var revisionNum     = 144;
+var jsonUpdatedTime = '24th of July, 2019';
+var revisionNum     = 145;
 
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (value) {
