f2070c1f04622c944b5d6ea3305faa1b47420dfc
Tom Plant <21111317+pl4nty@users.noreply.github.com>
2020-09-23 09:24:25 +1000

    Pre-load events for PoC

--- src/App.js
+++ src/App.js
@@ -10,107 +10,108 @@ const localizer = momentLocalizer(moment);
 
 class App extends Component {
   state = {
-    courses: ["Medicine 2"],
-    data: [
+    cacheStart: moment().endOf('week'),
+    cacheEnd: moment().startOf('week'),
+    modules: [
       {
-        key: 'john',
-        value: 'John Doe',
-      },
-      {
-        key: 'jane',
-        value: 'Jane Doe',
-      },
-      {
-        key: 'mary',
-        value: 'Mary Phillips',
-      },
-      {
-        key: 'robert',
-        value: 'Robert',
-      },
-      {
-        key: 'karius',
-        value: 'Karius',
+        key: "MEDI8020A_S1",
+        value: "MEDI8020A_S1 - Medicine 2"
       }
     ], // fetch("/courses.json").then(res => res.json()),
-    events: [
-      {
-        title: "Medicine 2",
-        start: new Date("2020-09-04 09:00:00"),
-        end: new Date("2020-09-04 10:00:00")
-      }
-    ]
+    events: []
   };
+
+  updateEvents(start, end) {
+    for (let module of this.state.modules) {
+      fetch(`${process.env.REACT_APP_ENDPOINT
+        }?ModuleName=${module.key
+        }&StartTime=${moment(start).format("YYYY-MM-DD")
+        }&EndTime=${moment(end).format("YYYY-MM-DD")}`, {
+        headers: {
+          'accept': 'application/json',
+          'authorization': `Basic ${process.env.REACT_APP_AUTH_KEY}`,
+          'x-ibm-client-id': process.env.REACT_APP_AUTH_ID,
+          'x-api-key': process.env.REACT_APP_DEV_KEY || ''
+        }
+      })
+        .then(res => res.json())
+        .then(
+          res => {
+            // Map and concat in one iteration for speed
+            let acc = this.state.events;
+            for (let session of res.TeachingSessions) {
+              // TODO add faculty or randomised (hash) colours
+              acc.push({
+                title: `${session.modulename} ${session.activitytype}`,
+                tooltipAccessor: event => "blah", //session.moduledescription
+                start: moment(session.teachingsessionstartdatetime).toDate(),
+                end: moment(session.teachingsessionenddatetime).toDate()
+              })
+            }
+
+            this.setState({
+              events: acc
+            }
+          )},
+          err => console.error(err)
+        )
+    }
+  }
+
+  componentDidMount(){
+    this.updateEvents(this.state.cacheStart, this.state.cacheEnd);
+  }
 
   render() {
     return (
       <div className="App">
         <h1 style={{height: "3vh"}}>Unofficial ANU Timetable</h1>
         <ReactSearchBox
-          placeholder="Placeholder"
-          value="Doe"
-          data={this.state.data}
+          placeholder="Search for a course here" //TODO localise
+          data={this.state.modules}
           callback={record => console.log(record)}
           style={{height: "5vh"}}
         />
         <button type="button">Click</button>
+
         <Calendar
           localizer={localizer}
-          formats={{
-            dayFormat: (date, culture) => localizer.format(date, 'dddd', culture),
-            eventTimeRangeFormat: () => ""
-          }}
+          popup={true}
           events={this.state.events}
-          defaultDate={new Date()}
+          style={{ height: "85vh" }}
           defaultView='work_week'
           views={['work_week']}
           min={moment().startOf("day").add(9, "hours").toDate()}
           max={moment().startOf("day").add(17, "hours").add(30, "minutes").toDate()}
-          style={{ height: "85vh" }}
-          onSelectEvent={event => {
+          formats={{
+            dayFormat: (date, culture) => localizer.format(date, 'dddd', culture),
+            eventTimeRangeFormat: () => ""
+          }}
+
+          // Delete on double click
+          onDoubleClickEvent={event => {
             let index = this.state.events.indexOf(event);
             if (index !== -1) {
               this.state.events.splice(index, 1)
             }
           }}
-          onRangeChange={range => {
-            for (let course of this.state.courses) {
-              let start = range[0];
-              let end = range[range.length-1];
 
-              fetch(`${process.env.REACT_APP_ENDPOINT}?ModuleDescription=${course
-                }&StartTime=${moment(start).format("YYYY-MM-DD")
-                }&EndTime=${moment(end).format("YYYY-MM-DD")}`, {
-                headers: {
-                  'accept': 'application/json',
-                  'authorization': `Basic ${process.env.REACT_APP_AUTH_KEY}`,
-                  'x-ibm-client-id': process.env.REACT_APP_AUTH_ID,
-                  'x-api-key': process.env.REACT_APP_DEV_KEY || ''
-                }
+          // Get events in new unloaded range
+          onRangeChange={range => {
+            let start = moment.min([this.state.cacheStart, moment(range[0]).subtract(1, 'week')]);
+            let end = moment.max([this.state.cacheEnd, moment(range[range.length-1]).add(1, 'week')]);
+
+            if (end !== this.state.cacheEnd || start !== this.state.cacheStart) {
+              this.setState({
+                ...this.state,
+                cacheStart: start,
+                cacheEnd: end
               })
-                .then(res => res.json())
-                .then(
-                  res => {
-                    console.log(res);
-                    console.log(this.state.events);
-                    let acc = [];
-                    for (let e of res.TeachingSessions) {
-                      // TODO add faculty or randomised (hash) colours
-                      acc.push({
-                        title: `${e.moduledescription} ${e.activitytype}`,
-                        start: new Date(e.teachingsessionstartdatetime),
-                        end: new Date(e.teachingsessionenddatetime)
-                      })
-                    }
-                    this.setState({
-                      events: acc,
-                      //eventsLoaded: true
-                    }
-                  )},
-                  err => console.error(err)
-                )
+  
+              this.updateEvents(start, end);
             }
           }}
+
           components={{
             event: this.customEvent,
             work_week: {
