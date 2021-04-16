3f1c04d4a4f188c94d3025fe04b99af2f0c95146
Tom Plant <21111317+pl4nty@users.noreply.github.com>
2021-04-16 18:04:53 +1000

    Added cache-based course search bar and localised

--- src/App.js
+++ src/App.js
@@ -1,78 +1,118 @@
 import React, { Component } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
-import ReactSearchBox from 'react-search-box';
+
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { format, parse, startOfWeek, endOfWeek, getDay, startOfDay, add, sub, isBefore, isAfter } from "date-fns";
+import { utcToZonedTime, zonedTimeToUtc } from "date-fns-tz";
+
+import ReactSearchBox from "react-search-box";
 
 import "./App.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
-const localizer = momentLocalizer(moment);
+const locales = {
+    'en-US': require('date-fns/locale/en-US'),
+}
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const anuTimeZone = "Australia/Canberra";
+const anuInitialTime = utcToZonedTime(new Date(), anuTimeZone);
 
 class App extends Component {
   state = {
-    cacheStart: moment().endOf('week'),
-    cacheEnd: moment().startOf('week'),
+    dayStart: add(startOfDay(anuInitialTime), {hours: 9}),
+    dayEnd: add(startOfDay(anuInitialTime), {hours: 17, minutes: 30}),
+    cacheStart: sub(startOfWeek(anuInitialTime), {weeks: 1}),
+    cacheEnd: add(endOfWeek(anuInitialTime), {weeks: 1}),
     modules: [
       {
         key: "MEDI8020A_S1",
         value: "MEDI8020A_S1 - Medicine 2"
       }
-    ], // fetch("/courses.json").then(res => res.json()),
-    events: []
+    ],
+    enrolled: [], // fetch("/courses.json").then(res => res.json()),
+    events: [{
+        title: `MEDI8020A_S1 Lecture`,
+        description: "Medicine 2",
+        start: zonedTimeToUtc("2021-04-15 09:00:00", anuTimeZone),
+        end: zonedTimeToUtc("2021-04-15 10:00:00", anuTimeZone)
+    }]
   };
 
-  updateEvents(start, end) {
-    for (let module of this.state.modules) {
-      fetch(`${process.env.REACT_APP_ENDPOINT
-        }?ModuleName=${module.key
-        }&StartTime=${moment(start).format("YYYY-MM-DD")
-        }&EndTime=${moment(end).format("YYYY-MM-DD")}`, {
-        headers: {
-          'accept': 'application/json',
-          'authorization': `Basic ${process.env.REACT_APP_AUTH_KEY}`,
-          'x-ibm-client-id': process.env.REACT_APP_AUTH_ID,
-          'x-api-key': process.env.REACT_APP_DEV_KEY || ''
-        }
-      })
-        .then(res => res.json())
-        .then(
-          res => {
-            // Map and concat in one iteration for speed
-            let acc = this.state.events;
-            for (let session of res.TeachingSessions) {
-              // TODO add faculty or randomised (hash) colours
-              acc.push({
-                title: `${session.modulename} ${session.activitytype}`,
-                tooltipAccessor: event => "blah", //session.moduledescription
-                start: moment(session.teachingsessionstartdatetime).toDate(),
-                end: moment(session.teachingsessionenddatetime).toDate()
-              })
-            }
+  addEvent(start, end, event) {
+    fetch(`${process.env.REACT_APP_ENDPOINT
+      }?ModuleName=${event
+      }&StartTime=${format(start, "yyyy-MM-dd")
+      }&EndTime=${format(end, "yyyy-MM-dd")}`, {
+      headers: {
+        'accept': 'application/json',
+        'authorization': `Basic ${process.env.REACT_APP_AUTH_KEY}`,
+        'x-ibm-client-id': process.env.REACT_APP_AUTH_ID,
+        'x-api-key': process.env.REACT_APP_DEV_KEY || ''
+      }
+    })
+      .then(res => res.json())
+      .then(
+        res => {
+          // Map and concat in one iteration for speed
+          let acc = this.state.events;
+          for (let session of res.TeachingSessions) {
+            // TODO add faculty or randomised (hash) colours
+            acc.push({
+              title: `${session.modulename} ${session.activitytype}`,
+              description: session.moduledescription,
+              start: utcToZonedTime(new Date(session.teachingsessionstartdatetime), anuTimeZone),
+              end: utcToZonedTime(new Date(session.teachingsessionenddatetime), anuTimeZone)
+            })
+          }
 
-            this.setState({
-              events: acc
-            }
-          )},
-          err => console.error(err)
-        )
+          this.setState({
+            events: acc
+          }
+        )},
+        err => console.error(err)
+      )
+  }
+
+  addEvents(start, end) {
+    for (let module of this.state.enrolled) {
+      this.addEvent(start, end, module)
     }
   }
 
   componentDidMount(){
-    this.updateEvents(this.state.cacheStart, this.state.cacheEnd);
+    console.log(this.state)
+    this.addEvents(this.state.cacheStart, this.state.cacheEnd);
   }
 
   render() {
     return (
       <div className="App">
-        <h1 style={{height: "3vh"}}>Unofficial ANU Timetable</h1>
+        <h1 style={{height: "2vh"}}>Unofficial ANU Timetable</h1>
+
+
         <ReactSearchBox
           placeholder="Search for a course here" //TODO localise
           data={this.state.modules}
-          callback={record => console.log(record)}
+          onSelect={record => {
+            let temp = this.state.enrolled
+            temp.push(record.key)
+            this.setState({
+              ...this.state,
+              enrolled: temp
+            })
+
+            this.addEvent(this.state.cacheStart, this.state.cacheEnd, record.key)
+          }}
           style={{height: "5vh"}}
         />
-        <button type="button">Click</button>
+        
+        
 
         <Calendar
           localizer={localizer}
@@ -81,12 +121,15 @@ class App extends Component {
           style={{ height: "85vh" }}
           defaultView='work_week'
           views={['work_week']}
-          min={moment().startOf("day").add(9, "hours").toDate()}
-          max={moment().startOf("day").add(17, "hours").add(30, "minutes").toDate()}
+          min={this.state.dayStart}
+          max={this.state.dayEnd}
           formats={{
-            dayFormat: (date, culture) => localizer.format(date, 'dddd', culture),
+            dayFormat: (date, culture) => localizer.format(date, 'EEEE', culture),
             eventTimeRangeFormat: () => ""
           }}
+
+          // Display descriptive name as tooltip
+          tooltipAccessor={event => event.description}
 
           // Delete on double click
           onDoubleClickEvent={event => {
@@ -96,19 +139,28 @@ class App extends Component {
             }
           }}
 
-          // Get events in new unloaded range
+          // Get events in new unloaded range - assumes pagination in only one direction
           onRangeChange={range => {
-            let start = moment.min([this.state.cacheStart, moment(range[0]).subtract(1, 'week')]);
-            let end = moment.max([this.state.cacheEnd, moment(range[range.length-1]).add(1, 'week')]);
+            console.log(range)
+            const newEnd = add(range[range.length-1], {weeks: 1});
+            console.log(newEnd)
 
-            if (end !== this.state.cacheEnd || start !== this.state.cacheStart) {
+            if (isAfter(newEnd, this.state.cacheEnd)) {
+              this.addEvents(this.state.cacheEnd, newEnd)
               this.setState({
                 ...this.state,
-                cacheStart: start,
-                cacheEnd: end
+                cacheEnd: newEnd
               })
-  
-              this.updateEvents(start, end);
+            } else {
+              const newStart = sub(range[0], {weeks: 1});
+
+              if (isBefore(newStart, this.state.cacheStart)) {
+                this.addEvents(newStart, this.state.cacheStart)
+                this.setState({
+                  ...this.state,
+                  cacheStart: newStart
+                })
+              } 
             }
           }}
 
