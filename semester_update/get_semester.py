2b026bccb93e2efc9bdd89b9a062210a2f1945f2
ZHANG Bo <zhangbo163126@outlook.com>
2021-08-07 13:08:35 +1000

    Update to use one one data file

--- semester_update/get_semester.py
+++ semester_update/get_semester.py
@@ -8,24 +8,34 @@ import os
 # Get current year
 # year = date.today().year
 year = 2022
-# Check if current year's data is already downloaded
-if not os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)),  str(year)+'.yml')):
-    # if checked, no need to run.
-    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),  str(year)+'.yml'), 'w') as f: 
-        # Get ANU offical academic calendar
-        # url= 'https://www.anu.edu.au/directories/university-calendar/' + str(year)+ '/calendar.ics'
+# Open the exist file.
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data.yml'), 'r') as f:
+    # Read from data.yml, get current database
+    l=yaml.load(f,Loader=yaml.SafeLoader)
+    try:
+        l[year]
+        flag=True
+    except TypeError:
+        flag=False
+    print(flag)
+    
+    
+if not flag:
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data.yml') , 'a+') as f: 
+        #Get ANU offical academic calendar
+        url= 'https://www.anu.edu.au/directories/university-calendar/' + str(year)+  '/calendar.ics'
+        #If current year's data is not downloaded, download it
         url='http://localhost:5500/calendar.ics'
         all_event=events(url, start=date(year, 1, 1), end=date(year, 12, 31))
         data={}
-        data={'Semester 1':{'start':'','end':''},'Semester 2':{'start':'','end':''}}
-
+        data={year:{'Semester 1':{'start':'','end':''},'Semester 2':{'start':'','end':''}}}
         for event in all_event:
             if event.summary == 'Semester 1 begins':
-                data['Semester 1']['start'] = event.start.date().isoformat()
+                data[year]['Semester 1']['start'] = event.start.date().isoformat()
             if event.summary=='Semester 2 begins':
-                data['Semester 2']['start']=event.start.date().isoformat()
+                data[year]['Semester 2']['start']=event.start.date().isoformat()
             if event.summary=='Semester 1 ends':
-                data['Semester 1']['end']=event.start.date().isoformat()
+                data[year]['Semester 1']['end']=event.start.date().isoformat()
             if event.summary=='Semester 2 ends':
-                data['Semester 2']['end']=event.start.date().isoformat()
-        yaml.dump(data, f)
+                data[year]['Semester 2']['end']=event.start.date().isoformat()
+        yaml.dump(data, f) 
