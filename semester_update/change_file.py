ef38808b616a9c69ac37027d0e3398abf07e1ac1
ZHANG Bo <zhangbo163126@outlook.com>
2021-08-07 16:17:40 +1000

    Update updater

--- semester_update/change_file.py
+++ semester_update/change_file.py
@@ -2,6 +2,7 @@ from shutil import copy
 from tempfile import TemporaryDirectory
 from datetime import date
 import os
+import yaml
 
 
 def inplace_change(filename, old_string, new_string):
@@ -20,21 +21,34 @@ def inplace_change(filename, old_string, new_string):
         f.write(s)
 
 
+def detect_semester():
+    today = date.today()
+    if 1 < today.month < 7:
+        return 'fall'
+    else:
+        return 'spring'
+
+
 today = date.today()
+
+with open('data.yaml', 'r') as f:
+    data = yaml.load(f, Loader=yaml.SafeLoader)
+    
 with TemporaryDirectory() as temp_dir:
     copy(os.path.join(os.path.dirname(__file__),
          'index_template.html'), os.path.join(temp_dir, 'index.html'))
     copy(os.path.join(os.path.dirname(__file__),
          'timetable_template.js'), os.path.join(temp_dir, 'timetable.js'))
     copy(os.path.join(os.path.dirname(__file__),
-         'scraper_template.py'), os.path.join(temp_dir, 'scraper.py'))
+         'scraper_template'), os.path.join(temp_dir, 'scraper.py'))
 
     inplace_change(os.path.join(temp_dir, 'scraper.py'),
                    '<%= year%>', str(today.year))
-    inplace_change(os.path.join(temp_dir, 'index.html'), '<%= year%>', str(today.year))
-    inplace_change(os.path.join(temp_dir, 'timetable.js'),
-                   'startday_template', str(today.year))
+    inplace_change(os.path.join(temp_dir, 'index.html'),
+                   '<%= year%>', str(today.year))
 
     copy(os.path.join(temp_dir, 'index.html'), os.path.realpath('..'))
-    copy(os.path.join(temp_dir, 'timetable.js'), os.path.join(os.path.realpath('..'),'js'))
-    copy(os.path.join(temp_dir, 'scraper.py'), os.path.join(os.path.realpath('..'),'anuscrape'))
+    copy(os.path.join(temp_dir, 'timetable.js'),
+         os.path.join(os.path.realpath('..'), 'js'))
+    copy(os.path.join(temp_dir, 'scraper.py'),
+         os.path.join(os.path.realpath('..'), 'anuscrape'))
