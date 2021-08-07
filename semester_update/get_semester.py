a83b354ea6430b8ebd47c6aa31c3649ffc943167
ZHANG Bo <zhangbo163126@outlook.com>
2021-08-07 14:29:42 +1000

    Update Detection; add scraper template

--- semester_update/get_semester.py
+++ semester_update/get_semester.py
@@ -6,36 +6,45 @@ import os
 # Create a folder for temporary files
 
 # Get current year
-# year = date.today().year
-year = 2022
-# Open the exist file.
+year = date.today().year
+# year = 2022
+# Open the exist file. PS: pyyaml can't read the file if using a+ mode
 with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data.yml'), 'r') as f:
     # Read from data.yml, get current database
-    l=yaml.load(f,Loader=yaml.SafeLoader)
+    l = yaml.load(f, Loader=yaml.SafeLoader)
+    # If the current year is in the database, end the program
     try:
         l[year]
-        flag=True
-    except TypeError:
-        flag=False
-    print(flag)
-    
-    
+        flag = True
+    except (TypeError, KeyError):
+        flag = False
+
+
 if not flag:
-    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data.yml') , 'a+') as f: 
-        #Get ANU offical academic calendar
-        url= 'https://www.anu.edu.au/directories/university-calendar/' + str(year)+  '/calendar.ics'
-        #If current year's data is not downloaded, download it
-        url='http://localhost:5500/calendar.ics'
-        all_event=events(url, start=date(year, 1, 1), end=date(year, 12, 31))
-        data={}
-        data={year:{'Semester 1':{'start':'','end':''},'Semester 2':{'start':'','end':''}}}
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data.yml'), 'a+') as f:
+        # Get ANU offical academic calendar
+        url = 'https://www.anu.edu.au/directories/university-calendar/' + \
+            str(year) + '/calendar.ics'
+        # If current year's data is not downloaded, download it
+        #url = 'http://localhost:5500/calendar.ics'
+        all_event = events(url, start=date(year, 1, 1), end=date(year, 12, 31))
+        # Structure the base
+        data = {year: {'Semester 1': {'start': '', 'end': ''},
+                       'Semester 2': {'start': '', 'end': ''}}}
         for event in all_event:
-            if event.summary == 'Semester 1 begins':
-                data[year]['Semester 1']['start'] = event.start.date().isoformat()
-            if event.summary=='Semester 2 begins':
-                data[year]['Semester 2']['start']=event.start.date().isoformat()
-            if event.summary=='Semester 1 ends':
-                data[year]['Semester 1']['end']=event.start.date().isoformat()
-            if event.summary=='Semester 2 ends':
-                data[year]['Semester 2']['end']=event.start.date().isoformat()
-        yaml.dump(data, f) 
+            # https://stackoverflow.com/questions/3389574/check-if-multiple-strings-exist-in-another-string/3389611
+            if ("semester" in event.summary.lower()) and not("research" in event.summary.lower()):
+                s1 = ["1", "one","first"]
+                start = ["starts", "begins"]
+                end = ["ends"]
+                s2 = ["2", "two","second"]
+                if any(x in event.summary.lower() for x in s1) and any(x in event.summary.lower() for x in start):
+                    data[year]['Semester 1']['start'] = event.start.date().isoformat()
+                if any(x in event.summary.lower() for x in s2) and any(x in event.summary.lower() for x in start):
+                    data[year]['Semester 2']['start'] = event.start.date().isoformat()
+                if any(x in event.summary.lower() for x in s1) and any(x in event.summary.lower() for x in end):
+                    data[year]['Semester 1']['end'] = event.start.date().isoformat()
+                if any(x in event.summary.lower() for x in s2) and any(x in event.summary.lower() for x in end):
+                    data[year]['Semester 2']['end'] = event.start.date().isoformat()
+        # write the data to the database
+        yaml.dump(data, f)
