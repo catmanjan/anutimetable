685eaa0192ddc2b29a3934f778e5e7ed632cf598
Tom Plant <21111317+pl4nty@users.noreply.github.com>
2022-01-12 23:24:24 +1100

    fix: export dropdown style and ICS download  (#40)
    * Show brand icons (fixes #38)
    * Actually download named ICS file

--- src/Export.js
+++ src/Export.js
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { Dropdown, DropdownButton, InputGroup } from 'react-bootstrap'
+import { SiApple, SiGooglecalendar, SiMicrosoftoutlook, SiMicrosoftexchange } from 'react-icons/si'
+import { RiCalendar2Fill } from 'react-icons/ri'
 
 const Export = ({ API }) => {
   const [webcal, setWebcal] = useState(`${API}/GetICS${window.location.search}`)
@@ -11,22 +13,24 @@ const Export = ({ API }) => {
     onClick={() => setWebcal(`${API}/GetICS${window.location.search}`)}
   >
     <Dropdown.Item eventKey="web" target={"_blank"} rel={"noreferrer"}
-      href={webcal.replace(/(http|https)/,'webcal')}>WebCal (eg iOS)</Dropdown.Item>
+      href={webcal.replace(/(http|https)/,'webcal')}>
+      <SiApple /> WebCal (eg iOS)
+    </Dropdown.Item>
     <Dropdown.Item eventKey="gcal" target={"_blank"} rel={"noreferrer"} 
       href={`https://www.google.com/calendar/render?cid=${webcal}`}>
-      Google Calendar
+      <SiGooglecalendar /> Google Calendar
     </Dropdown.Item>
     <Dropdown.Item eventKey="msol" target={"_blank"} rel={"noreferrer"}
       href={`https://outlook.live.com/calendar/addfromweb?name=ANU&url=${webcal}`}>
-      Outlook.com
+      <SiMicrosoftoutlook /> Outlook.com
     </Dropdown.Item>
     <Dropdown.Item eventKey="aad" target={"_blank"} rel={"noreferrer"}
       href={`https://outlook.office.com/calendar/addfromweb?name=ANU&url=${webcal}`}>
-      Office 365
+      <SiMicrosoftexchange /> Office 365
     </Dropdown.Item>
     <Dropdown.Divider />
-    <Dropdown.Item eventKey="ics" target={"_blank"} rel={"noreferrer"} href={webcal}>
-      Download ICS file
+    <Dropdown.Item eventKey="ics" download={`ANU Timetable ${new Date().toLocaleDateString().replace(/(\/|\\)/g,'.')}.ics`} href={webcal}>
+      <RiCalendar2Fill /> Download ICS file
     </Dropdown.Item>
   </DropdownButton>
 }
